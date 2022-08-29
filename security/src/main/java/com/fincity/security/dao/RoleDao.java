526d5279d01fa28dd93173c724b88aeaa280e43c
Surendhar Satyavarapu <surendhar.s@fincity.com>
2022-08-29 15:38:39 +0530

    Role Service

--- security/src/main/java/com/fincity/security/dao/RoleDao.java
+++ security/src/main/java/com/fincity/security/dao/RoleDao.java
@@ -2,16 +2,23 @@ package com.fincity.security.dao;
 
 import static com.fincity.security.jooq.tables.SecurityRole.SECURITY_ROLE;
 
+import org.jooq.Field;
 import org.jooq.types.ULong;
+import org.springframework.stereotype.Component;
 
-import com.fincity.saas.commons.jooq.dao.AbstractUpdatableDAO;
 import com.fincity.security.dto.Role;
 import com.fincity.security.jooq.tables.records.SecurityRoleRecord;
 
-public class RoleDao extends AbstractUpdatableDAO<SecurityRoleRecord, ULong, Role> {
+@Component
+public class RoleDao extends AbstractClientCheckDAO<SecurityRoleRecord, ULong, Role> {
 
 	public RoleDao() {
 		super(Role.class, SECURITY_ROLE, SECURITY_ROLE.ID);
+	}
+
+	@Override
+	protected Field<ULong> getClientIDField() {
+		return SECURITY_ROLE.CLIENT_ID;
 	}
 
 }
