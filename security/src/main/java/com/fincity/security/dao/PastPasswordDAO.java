efdd95cc8459974cc58eb1c2c884bdf687a0b0bf
Surendhar Satyavarapu <surendhar.s@fincity.com>
2022-11-18 17:15:10 +0530

    Added changes to support past...
    ...passwords service file and dao to support addition and deletion of past passwords from respective table.

--- security/src/main/java/com/fincity/security/dao/PastPasswordDAO.java
+++ security/src/main/java/com/fincity/security/dao/PastPasswordDAO.java
@@ -3,7 +3,9 @@ package com.fincity.security.dao;
 import static com.fincity.security.jooq.tables.SecurityPastPasswords.SECURITY_PAST_PASSWORDS;
 
 import java.util.List;
+import java.util.Objects;
 
+import org.jooq.DeleteQuery;
 import org.jooq.Field;
 import org.jooq.Record1;
 import org.jooq.Table;
@@ -27,9 +29,36 @@ public class PastPasswordDAO extends AbstractDAO<SecurityPastPasswordsRecord, UL
 
 		return Flux.from(this.dslContext.select(SECURITY_PAST_PASSWORDS.PASSWORD)
 		        .from(SECURITY_PAST_PASSWORDS)
-		        .where(SECURITY_PAST_PASSWORDS.ID.eq(userId)))
+		        .where(SECURITY_PAST_PASSWORDS.USER_ID.eq(userId))
+		        .orderBy(SECURITY_PAST_PASSWORDS.CREATED_AT.asc()))
 		        .map(Record1::value1)
 		        .collectList();
+	}
+
+	public Mono<Boolean> deletePastPasswordBasedOnUserId(ULong userId) {
+
+		Mono<ULong> deletableId = Mono.from(this.dslContext.select(SECURITY_PAST_PASSWORDS.ID)
+		        .from(SECURITY_PAST_PASSWORDS)
+		        .where(SECURITY_PAST_PASSWORDS.USER_ID.eq(userId))
+		        .orderBy(SECURITY_PAST_PASSWORDS.CREATED_AT.asc())
+		        .limit(1))
+		        .map(Record1::value1);
+
+		DeleteQuery<SecurityPastPasswordsRecord> query = this.dslContext.deleteQuery(SECURITY_PAST_PASSWORDS);
+
+		return deletableId.map(id -> {
+			query.addConditions(SECURITY_PAST_PASSWORDS.ID.eq(id));
+			return Mono.from(query);
+		})
+		        .map(Objects::nonNull);
+	}
+
+	public Mono<Integer> fetchPasswordsCount(ULong userId) {
+
+		return Mono.from(this.dslContext.selectCount()
+		        .from(SECURITY_PAST_PASSWORDS)
+		        .where(SECURITY_PAST_PASSWORDS.USER_ID.eq(userId)))
+		        .map(Record1::value1);
 	}
 
 }
--- security/src/main/java/com/fincity/security/service/PastPasswordService.java
+++ security/src/main/java/com/fincity/security/service/PastPasswordService.java
@@ -1,15 +1,86 @@
 package com.fincity.security.service;
 
+import static com.fincity.nocode.reactor.util.FlatMapUtil.flatMapMono;
+
+import java.util.List;
+import java.util.Objects;
+
 import org.jooq.types.ULong;
+import org.jooq.types.UShort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fincity.saas.common.security.util.SecurityContextUtil;
 import com.fincity.saas.commons.jooq.service.AbstractJOOQDataService;
 import com.fincity.security.dao.PastPasswordDAO;
 import com.fincity.security.dto.PastPassword;
 import com.fincity.security.jooq.tables.records.SecurityPastPasswordsRecord;
 
+import reactor.core.publisher.Mono;
+
 @Service
 public class PastPasswordService
         extends AbstractJOOQDataService<SecurityPastPasswordsRecord, ULong, PastPassword, PastPasswordDAO> {
 
+	@Autowired
+	private ClientService clientService;
+
+	public Mono<UShort> getHistoryCount(ULong clientId) {
+
+		return flatMapMono(
+
+		        () -> clientService.getClientPasswordPolicy(clientId),
+
+		        passwordPolicy -> Mono.just(passwordPolicy.getPassHistoryCount()));
+
+	}
+
+	// delete past password
+
+	public Mono<List<String>> fetchList(ULong userId) {
+		return this.dao.fetchPasswordsById(userId);
+	}
+
+	public Mono<Integer> getCountForSelectedUser(ULong userId) {
+		return this.dao.fetchPasswordsCount(userId);
+	}
+
+	public Mono<Boolean> addPassword(PastPassword pastPassword) {
+		return this.dao.create(pastPassword)
+		        .map(Objects::nonNull);
+	}
+
+	// add new password based on history count which was applicable for that
+	// selected user id
+
+	public Mono<Boolean> addBasedOnHistoryCount(PastPassword pastPassword) {
+
+		return flatMapMono(
+
+		        SecurityContextUtil::getUsersClientId,
+
+		        clientId -> Mono.just(pastPassword.getUserId()),
+
+		        (clientId, userId) -> this.getCountForSelectedUser(userId),
+
+		        (clientId, userId, pastPasswordCount) -> this.getHistoryCount(ULong.valueOf(clientId)),
+
+		        (clientId, userId, pastPasswordCount, historyCount) ->
+				{
+			        if (pastPasswordCount.intValue() < historyCount.intValue())
+				        return this.dao.create(pastPassword)
+				                .map(Objects::nonNull);
+
+			        return deletePastPassword(userId);
+		        }
+
+		);
+
+	}
+
+	// delete past password record based on oldest time stamp for selected userId
+	public Mono<Boolean> deletePastPassword(ULong userId) {
+
+		return this.dao.deletePastPasswordBasedOnUserId(userId);
+	}
 }
