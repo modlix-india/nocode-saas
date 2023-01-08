30dcb68ad7e243eca2cc60eb4653d7fadf4a7dd6
serialcoder13 <kiran.grandhi@fincity.com>
2023-01-08 18:13:02 +0530

    Updated the connection pooling

--- commons-jooq/src/main/java/com/fincity/saas/commons/jooq/configuration/AbstractJooqBaseConfiguration.java
+++ commons-jooq/src/main/java/com/fincity/saas/commons/jooq/configuration/AbstractJooqBaseConfiguration.java
@@ -2,23 +2,48 @@ package com.fincity.saas.commons.jooq.configuration;
 
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import com.fincity.saas.commons.configuration.AbstractBaseConfiguration;
 import com.fincity.saas.commons.configuration.service.AbstractMessageService;
 
+import io.r2dbc.pool.ConnectionPool;
+import io.r2dbc.pool.ConnectionPoolConfiguration;
+import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import io.r2dbc.spi.ConnectionFactoryOptions.Builder;
 
 public class AbstractJooqBaseConfiguration extends AbstractBaseConfiguration {
 
-	
+	@Value("${spring.r2dbc.url}")
+	private String url;
+
+	@Value("${spring.r2dbc.username}")
+	private String username;
+
+	@Value("${spring.r2dbc.password}")
+	private String password;
+
 	public void initialize(AbstractMessageService messageResourceService) {
 		super.initialize();
-		this.objectMapper.registerModule(new com.fincity.saas.commons.jooq.jackson.UnsignedNumbersSerializationModule(messageResourceService));
+		this.objectMapper.registerModule(
+		        new com.fincity.saas.commons.jooq.jackson.UnsignedNumbersSerializationModule(messageResourceService));
 	}
-	
+
 	@Bean
-	DSLContext context(ConnectionFactory factory) {
-		return DSL.using(factory);
+	DSLContext context() {
+		
+		Builder props = ConnectionFactoryOptions.parse(url)
+		        .mutate();
+		ConnectionFactory factory = ConnectionFactories.get(props
+		        .option(ConnectionFactoryOptions.DRIVER, "pool")
+		        .option(ConnectionFactoryOptions.PROTOCOL, "mysql")
+		        .option(ConnectionFactoryOptions.USER, username)
+		        .option(ConnectionFactoryOptions.PASSWORD, password)
+		        .build());
+		return DSL.using(new ConnectionPool(ConnectionPoolConfiguration.builder(factory)
+		        .build()));
 	}
 }
