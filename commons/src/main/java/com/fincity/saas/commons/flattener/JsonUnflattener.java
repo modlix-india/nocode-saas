f6116294133cbd2287aa9e4a668f15f0d667a99c
Surendhar Satyavarapu <surendhar.s@fincity.com>
2023-03-24 17:55:54 +0530

    Updated unflattener method

--- commons/src/main/java/com/fincity/saas/commons/flattener/JsonUnflattener.java
+++ commons/src/main/java/com/fincity/saas/commons/flattener/JsonUnflattener.java
@@ -44,63 +44,74 @@ public class JsonUnflattener {
 
                 String part = parts[i];
 
-                if (part.indexOf('[') == -1) {
-
-                    if (!pointer.isJsonObject())
-                        throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, OBJECT_NOT_FOUND);
-
-                    JsonObject mini = pointer.getAsJsonObject();
-
-                    if (!mini.has(part))
-                        mini.add(part, new JsonObject());
-
-                    pointer = mini.get(part);
-                } else {
-
-                    String[] subParts = part.split("\\[");
-
-                    int j = 0;
-                    JsonObject mini = pointer.getAsJsonObject();
-
-                    if (!mini.has(subParts[j]))
-                        mini.add(subParts[j], new JsonArray());
-
-                    pointer = mini.get(subParts[j]);
-
-                    for (j = 1; j < subParts.length - 1; j++) {
-
-                        if (!pointer.isJsonArray())
-                            throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, ARRAY_NOT_FOUND);
-
-                        JsonArray miniArray = pointer.getAsJsonArray();
-                        int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
-                        if (index >= miniArray.size()) {
-
-                            int size = miniArray.size();
-                            for (int k = 0; k < (index - size); k++)
-                                miniArray.add(new JsonArray());
-                        }
-
-                        pointer = miniArray.get(index);
-                    }
-
-                    int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
-                    JsonArray miniArray = pointer.getAsJsonArray();
-                    if (index >= miniArray.size()) {
-
-                        int size = miniArray.size();
-                        for (int k = 0; k <= (index - size); k++)
-                            miniArray.add(new JsonObject());
-                    }
-
-                    pointer = miniArray.get(index);
-                }
+                if (part.indexOf('[') == -1)
+                    pointer = processObjectOperator(pointer, part);
+                else
+                    pointer = processArrayOperator(pointer, part);
             }
 
             placeValue(parts[i], pointer, entry.getValue(), flattenedSchemaType.get(entry.getKey()));
         }
 
         return job;
+    }
+
+    private static JsonElement processObjectOperator(JsonElement pointer, String part) {
+
+        if (!pointer.isJsonObject())
+            throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, OBJECT_NOT_FOUND);
+
+        JsonObject mini = pointer.getAsJsonObject();
+
+        if (!mini.has(part))
+            mini.add(part, new JsonObject());
+
+        pointer = mini.get(part);
+
+        return pointer;
+    }
+
+    private static JsonElement processArrayOperator(JsonElement pointer, String part) {
+
+        String[] subParts = part.split("\\[");
+
+        int j = 0;
+        JsonObject mini = pointer.getAsJsonObject();
+
+        if (!mini.has(subParts[j]))
+            mini.add(subParts[j], new JsonArray());
+
+        pointer = mini.get(subParts[j]);
+
+        for (j = 1; j < subParts.length - 1; j++) {
+
+            if (!pointer.isJsonArray())
+                throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, ARRAY_NOT_FOUND);
+
+            JsonArray miniArray = pointer.getAsJsonArray();
+            int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
+            if (index >= miniArray.size()) {
+
+                int size = miniArray.size();
+                for (int k = 0; k < (index - size); k++)
+                    miniArray.add(new JsonArray());
+            }
+
+            pointer = miniArray.get(index);
+        }
+
+        int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
+        JsonArray miniArray = pointer.getAsJsonArray();
+        if (index >= miniArray.size()) {
+
+            int size = miniArray.size();
+            for (int k = 0; k <= (index - size); k++)
+                miniArray.add(new JsonObject());
+        }
+
+        pointer = miniArray.get(index);
+
+        return pointer;
     }
 
     private static void placeValue(String part, JsonElement pointer, String value,
@@ -112,96 +123,82 @@ public class JsonUnflattener {
                 throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, OBJECT_NOT_FOUND);
 
             if (!StringUtil.safeIsBlank(value)) {
-                if (schemaTypes.contains(SchemaType.INTEGER))
-                    pointer.getAsJsonObject().addProperty(part, Integer.valueOf(value));
-
-                else if (schemaTypes.contains(SchemaType.LONG))
-                    pointer.getAsJsonObject().addProperty(part, Long.valueOf(value));
-
-                else if (schemaTypes.contains(SchemaType.FLOAT))
-                    pointer.getAsJsonObject().addProperty(part, Float.valueOf(value));
-
-                else if (schemaTypes.contains(SchemaType.DOUBLE))
-                    pointer.getAsJsonObject().addProperty(part, Double.valueOf(value));
-
-                else if (schemaTypes.contains(SchemaType.STRING))
-                    pointer.getAsJsonObject().addProperty(part, value);
-
-                else if (schemaTypes.contains(SchemaType.BOOLEAN))
-                    pointer.getAsJsonObject().addProperty(part, Boolean.valueOf(value));
-
-                else if (schemaTypes.contains(SchemaType.NULL))
-                    throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, NULL_VALUE_EXPECTED);
-
-                else
-                    pointer.getAsJsonObject().add(part, JsonNull.INSTANCE);
-
+                pointer.getAsJsonObject().add(part, getJSONElementBySchemaType(schemaTypes, value));
             }
 
         } else {
+            processArrayOperatorForPlaceValue(part, pointer, value, schemaTypes);
+        }
+    }
 
-            String[] subParts = part.split("\\[");
+    private static void processArrayOperatorForPlaceValue(String part, JsonElement pointer, String value,
+            Set<SchemaType> schemaTypes) {
+        
+        String[] subParts = part.split("\\[");
 
-            int j = 0;
-            JsonObject mini = pointer.getAsJsonObject();
+        int j = 0;
+        JsonObject mini = pointer.getAsJsonObject();
 
-            if (!mini.has(subParts[j]))
-                mini.add(subParts[j], new JsonObject());
+        if (!mini.has(subParts[j]))
+            mini.add(subParts[j], new JsonObject());
 
-            pointer = mini.get(subParts[j]);
+        pointer = mini.get(subParts[j]);
 
-            for (j = 1; j < subParts.length - 1; j++) {
+        for (j = 1; j < subParts.length - 1; j++) {
 
-                if (!pointer.isJsonArray())
-                    throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, ARRAY_NOT_FOUND);
+            if (!pointer.isJsonArray())
+                throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, ARRAY_NOT_FOUND);
 
-                JsonArray miniArray = pointer.getAsJsonArray();
-                int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
-                if (index >= miniArray.size()) {
-
-                    int size = miniArray.size();
-                    for (int k = 0; k <= (index - size); k++)
-                        miniArray.add(new JsonArray());
-                }
-
-                pointer = miniArray.get(index);
-            }
-
-            int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
             JsonArray miniArray = pointer.getAsJsonArray();
+            int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
             if (index >= miniArray.size()) {
 
                 int size = miniArray.size();
-                for (int k = 0; k < (index - size); k++)
-                    miniArray.add(JsonNull.INSTANCE);
+                for (int k = 0; k <= (index - size); k++)
+                    miniArray.add(new JsonArray());
             }
 
-            if (!StringUtil.safeIsBlank(value)) {
-                if (schemaTypes.contains(SchemaType.INTEGER))
-                    miniArray.set(index, new JsonPrimitive(Integer.valueOf(value)));
-
-                else if (schemaTypes.contains(SchemaType.LONG))
-                    miniArray.set(index, new JsonPrimitive(Long.valueOf(value)));
-
-                else if (schemaTypes.contains(SchemaType.FLOAT))
-                    miniArray.set(index, new JsonPrimitive(Float.valueOf(value)));
-
-                else if (schemaTypes.contains(SchemaType.DOUBLE))
-                    miniArray.set(index, new JsonPrimitive(Double.valueOf(value)));
-
-                else if (schemaTypes.contains(SchemaType.STRING))
-                    miniArray.set(index, new JsonPrimitive(String.valueOf(value)));
-
-                else if (schemaTypes.contains(SchemaType.BOOLEAN))
-                    miniArray.set(index, new JsonPrimitive(Boolean.valueOf(value)));
-
-                else if (schemaTypes.contains(SchemaType.NULL))
-                    throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, NULL_VALUE_EXPECTED);
-
-                else
-                    miniArray.set(index, JsonNull.INSTANCE);
-
-            }
+            pointer = miniArray.get(index);
         }
+
+        int index = Integer.parseInt(subParts[j].substring(0, subParts[j].length() - 1));
+        JsonArray miniArray = pointer.getAsJsonArray();
+        if (index >= miniArray.size()) {
+
+            int size = miniArray.size();
+            for (int k = 0; k < (index - size); k++)
+                miniArray.add(JsonNull.INSTANCE);
+        }
+
+        if (!StringUtil.safeIsBlank(value)) {
+            miniArray.set(index, getJSONElementBySchemaType(schemaTypes, value));
+        }
+    }
+
+    private static JsonElement getJSONElementBySchemaType(Set<SchemaType> schemaTypes, String value) {
+
+        if (schemaTypes.contains(SchemaType.INTEGER))
+            return new JsonPrimitive(Integer.valueOf(value));
+
+        else if (schemaTypes.contains(SchemaType.LONG))
+            return new JsonPrimitive(Long.valueOf(value));
+
+        else if (schemaTypes.contains(SchemaType.FLOAT))
+            return new JsonPrimitive(Float.valueOf(value));
+
+        else if (schemaTypes.contains(SchemaType.DOUBLE))
+            return new JsonPrimitive(Double.valueOf(value));
+
+        else if (schemaTypes.contains(SchemaType.STRING))
+            return new JsonPrimitive(value);
+
+        else if (schemaTypes.contains(SchemaType.BOOLEAN))
+            return new JsonPrimitive(Boolean.valueOf(value));
+
+        else if (schemaTypes.contains(SchemaType.NULL))
+            throw new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, NULL_VALUE_EXPECTED);
+
+        else
+            return JsonNull.INSTANCE;
     }
 }
--- commons/src/test/java/com/fincity/saas/commons/flattener/JsonUnflattenerTest.java
+++ commons/src/test/java/com/fincity/saas/commons/flattener/JsonUnflattenerTest.java
@@ -3,55 +3,62 @@ package com.fincity.saas.commons.flattener;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.junit.jupiter.api.Test;
 
+import com.fincity.nocode.kirun.engine.json.schema.type.SchemaType;
+import com.fincity.nocode.kirun.engine.json.schema.type.Type;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 class JsonUnflattenerTest {
 
     @Test
     void test() {
-        Map<String, Object> flatList = Map.of("professionalDetails.address.building_name", "sri",
-                "professionalDetails.address.pincode", 533003, "professionalDetails.address.city", "bnglr",
-                "basicDetails.phone", 1234.1,
-                "userType", "ACTIVE", "parentsName[0].firstName", "kiran", "parentsName[1].firstName", "kumar",
-                "parentsName[0].a[0].b", "bvalue", "parentsName[0].a[1].b", "cValue");
+        Map<String, String> flatList = Map.of("professionalDetails.address.building_name", "sri",
+                "professionalDetails.address.pincode", "533003", "professionalDetails.address.city", "bnglr",
+                "basicDetails.phone", "12341",
+                "userType", "ACTIVE", "parentsName[0].firstName", "kiran", "parentsName[1].firstName", "kumar");
+
+        var ja = new JsonArray();
+        var temp1 = new JsonObject();
+        temp1.add("firstName", new JsonPrimitive("kiran"));
+
+        var temp2 = new JsonObject();
+        temp2.add("firstName", new JsonPrimitive("kumar"));
+        ja.add(temp1);
+        ja.add(temp2);
 
         JsonObject jsono = new JsonObject();
         jsono.add("professionalDetails", new JsonObject());
+        jsono.add("parentsName", ja);
         jsono.add("basicDetails", new JsonObject());
         jsono.addProperty("userType", "ACTIVE");
         jsono.getAsJsonObject("professionalDetails").add("address", new JsonObject());
-        jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").addProperty("building_name", "sri");
-//                new JsonPrimitive("building_name"));
-        jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").addProperty("pincode", "533003");
-//                new JsonPrimitive("pincode"));
         jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").addProperty("city", "bnglr");
-//                new JsonPrimitive("city"));
-        jsono.getAsJsonObject("basicDetails").addProperty("phone", "1234");
-        System.out.println(jsono);
+        jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").addProperty("building_name", "sri");
+        jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").addProperty("pincode", 533003);
+        jsono.getAsJsonObject("basicDetails").addProperty("phone", 12341);
 
-        JsonObject result = JsonUnflattener.unflatten(flatList);
-        System.out.println(result);
+        Map<String, Set<SchemaType>> propsSchema = new TreeMap<>();
+        propsSchema.put("professionalDetails.address.building_name",
+                Type.of(SchemaType.STRING).getAllowedSchemaTypes());
+        propsSchema.put("professionalDetails.address.pincode", Type.of(SchemaType.LONG).getAllowedSchemaTypes());
+        propsSchema.put("professionalDetails.address.city", Type.of(SchemaType.STRING).getAllowedSchemaTypes());
+        propsSchema.put("basicDetails.phone", Type.of(SchemaType.INTEGER).getAllowedSchemaTypes());
+        propsSchema.put("userType", Type.of(SchemaType.STRING).getAllowedSchemaTypes());
+        propsSchema.put("parentsName[0].firstName", Type.of(SchemaType.STRING).getAllowedSchemaTypes());
+        propsSchema.put("parentsName[1].firstName", Type.of(SchemaType.STRING).getAllowedSchemaTypes());
+        propsSchema.put("parentsName[0].a[0].b", Type.of(SchemaType.STRING).getAllowedSchemaTypes());
+        propsSchema.put("parentsName[0].a[1].b", Type.of(SchemaType.BOOLEAN).getAllowedSchemaTypes());
+
+        JsonObject result = JsonUnflattener.unflatten(flatList, propsSchema);
+
+
         assertEquals(jsono, result);
     }
-
-//    @Test
-//    void test2() {
-//        List<String> flatList = List.of("professionalDetails.address.building_name",
-//                "professionalDetails.address.pincode");
-//        ,"parentsName[0].firstName","parentsName[1].firstName","parentsName[0].a[0].b","parentsName[0].a[0].c");
-
-//        JsonObject jsono = new JsonObject();
-//        jsono.add("professionalDetails", new JsonObject());
-//        jsono.getAsJsonObject("professionalDetails").add("address", new JsonObject());
-//        jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").add("building_name", new JsonObject());
-//        jsono.getAsJsonObject("professionalDetails").getAsJsonObject("address").add("pincode", new JsonObject());
-//        System.out.println(jsono);
-//        JsonObject result = JsonUnflattener.unflatten(flatList, new JsonObject());
-//        System.out.println(result);
-//        assertEquals(jsono, result);
-//    }
 
 }
