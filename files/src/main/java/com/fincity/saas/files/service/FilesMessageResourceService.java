cf8254c0e0bbb851a26507e153aa0c4f307d8db7
Surendhar Satyavarapu <surendhar.s@fincity.com>
2023-04-10 15:08:27 +0530

    Removed unnecessary trigger messages

--- files/src/main/java/com/fincity/saas/files/service/FilesMessageResourceService.java
+++ files/src/main/java/com/fincity/saas/files/service/FilesMessageResourceService.java
@@ -39,7 +39,6 @@ public class FilesMessageResourceService extends AbstractMessageService {
 	public static final String TIME_UNIT_ERROR = "time_unit_error";
 	public static final String TIME_SPAN_ERROR = "time_span_error";
 	public static final String SECURED_KEY_CREATION_ERROR = "secured_key_creation_error";
-	public static final String FOLDER_CREATION_ERROR = "folder_creation_error";
 
 	public FilesMessageResourceService() {
 
