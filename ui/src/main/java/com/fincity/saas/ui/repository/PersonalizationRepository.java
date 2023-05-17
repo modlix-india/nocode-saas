5efab17c3b1a9f9ef6f0a2ddc0c0ec5f0da0c3d8
serialcoder13 <kiran.grandhi@fincity.com>
2023-05-17 22:19:22 +0530

    Repositories are fixed by removing the unnecessary repository annotations.

--- ui/src/main/java/com/fincity/saas/ui/repository/PersonalizationRepository.java
+++ ui/src/main/java/com/fincity/saas/ui/repository/PersonalizationRepository.java
@@ -1,13 +1,10 @@
 package com.fincity.saas.ui.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.fincity.saas.commons.mongo.repository.IOverridableDataRepository;
 import com.fincity.saas.ui.document.Personalization;
 
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface PersonalizationRepository extends IOverridableDataRepository<Personalization> {
 
 	public Mono<Personalization> findOneByNameAndAppCodeAndCreatedBy(String name, String applicationName,
--- ui/src/main/java/com/fincity/saas/ui/repository/UISchemaDocumentRepository.java
+++ ui/src/main/java/com/fincity/saas/ui/repository/UISchemaDocumentRepository.java
@@ -1,11 +1,8 @@
 package com.fincity.saas.ui.repository;
 
-import org.springframework.stereotype.Repository;
-
 import com.fincity.saas.commons.mongo.repository.IOverridableDataRepository;
 import com.fincity.saas.ui.document.UISchema;
 
-@Repository
 public interface UISchemaDocumentRepository extends IOverridableDataRepository<UISchema> {
 
 }
