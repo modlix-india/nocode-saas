f0489c5571ee55833fa9288847a6cad7824f3234
serialcoder13 <kiran.grandhi@fincity.com>
2023-05-17 22:17:57 +0530

    Eureka client is missing and it is fixed.

--- admin/src/main/java/com/fincity/admin/AdminServerApplication.java
+++ admin/src/main/java/com/fincity/admin/AdminServerApplication.java
@@ -2,13 +2,13 @@ package com.fincity.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
-
+@EnableEurekaClient
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
