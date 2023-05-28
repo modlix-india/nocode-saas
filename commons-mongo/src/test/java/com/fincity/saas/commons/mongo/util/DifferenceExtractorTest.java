ddc6ad16be989f5552406688cfe069ff4f140202
serialcoder13 <kiran.grandhi@fincity.com>
2023-05-28 12:21:18 +0530

    Worked on Difference extractor test to test other cases

--- commons-mongo/src/test/java/com/fincity/saas/commons/mongo/util/DifferenceExtractorTest.java
+++ commons-mongo/src/test/java/com/fincity/saas/commons/mongo/util/DifferenceExtractorTest.java
@@ -1,4 +1,4 @@
-package com.fincity.saas.ui.util;
+package com.fincity.saas.commons.mongo.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -7,12 +7,34 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import com.fincity.saas.commons.mongo.util.DifferenceExtractor;
+import com.fincity.saas.commons.util.HashMapUtil;
 
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 class DifferenceExtractorTest {
+
+	@Test
+	void testExtractObjectObject() {
+
+		Map<String, Object> base = Map.of("a", 20, "b", 30, "c", Map.of("k", 1, "i", 2), "d", Map.of("k", 1, "i", 2),
+		        "e", Map.of("k", 1, "i", 2));
+
+		Map<String, Object> inc = Map.of("a", 20, "c", Map.of("k", 1, "i", 2), "d", Map.of("k", 2), "f",
+		        Map.of("k", 2, "i", 3));
+
+		Map<String, Object> expected = new HashMap<>();
+		expected.put("b", null);
+		expected.put("d", HashMapUtil.of("i", null, "k", 2));
+		expected.put("e", null);
+		expected.put("f", Map.of("k", 2, "i", 3));
+
+		Mono<Map<String, ?>> x = DifferenceExtractor.extract(inc, base);
+
+		StepVerifier.create(x)
+		        .assertNext(y -> assertEquals(expected, y))
+		        .verifyComplete();
+	}
 
 	@Test
 	void testExtractMap1() {
@@ -37,14 +59,25 @@ class DifferenceExtractorTest {
 		Map<String, Object> incoming = Map.of("check", "what", "checkMap", Map.of("x", 12, "y", Map.of("a", 12)));
 		Map<String, Object> base = Map.of("check", "what", "checkMap", Map.of("x", 2, "y", Map.of("a", 3)));
 
-		System.out.println(incoming);
-		System.out.println(base);
-
 		Mono<Map<String, ?>> x = DifferenceExtractor.extract(incoming, base);
 
 		HashMap<String, Object> expected = new HashMap<>();
 		expected.put("checkMap", (Object) Map.of("x", 12, "y", Map.of("a", 12)));
-		System.out.println(expected);
+
+		StepVerifier.create(x)
+		        .assertNext(y -> assertEquals(expected, y))
+		        .verifyComplete();
+	}
+
+	@Test
+	void testExtractSame() {
+
+		Map<String, Object> incoming = Map.of("check", "what", "checkMap", Map.of("x", 12, "y", Map.of("a", 12)));
+		Map<String, Object> base = Map.of("check", "what", "checkMap", Map.of("x", 12, "y", Map.of("a", 12)));
+
+		Mono<Map<String, ?>> x = DifferenceExtractor.extract(incoming, base);
+
+		Map<String, Object> expected = Map.of();
 
 		StepVerifier.create(x)
 		        .assertNext(y -> assertEquals(expected, y))
