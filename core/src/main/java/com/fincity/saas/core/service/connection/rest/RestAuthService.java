f220d73d87f021281f2f0efcdf95c243944eeabc
Vishwas Saini <vishwas.saini@fincity.com>
2024-05-23 02:10:24 +0530

    Add Rest Oauth Service Added null header check

--- core/src/main/java/com/fincity/saas/core/service/connection/rest/RestAuthService.java
+++ core/src/main/java/com/fincity/saas/core/service/connection/rest/RestAuthService.java
@@ -2,14 +2,16 @@ package com.fincity.saas.core.service.connection.rest;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 
 import com.fincity.nocode.reactor.util.FlatMapUtil;
 import com.fincity.saas.commons.exeception.GenericException;
-import com.fincity.saas.commons.mongo.service.AbstractMongoMessageResourceService;
+import com.fincity.saas.commons.util.LogUtil;
 import com.fincity.saas.core.dao.CoreTokenDAO;
 import com.fincity.saas.core.document.Connection;
 import com.fincity.saas.core.dto.CoreToken;
@@ -21,12 +23,15 @@ import com.fincity.saas.core.service.CoreMessageResourceService;
 import com.google.gson.JsonElement;
 
 import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
 @Service
 public class RestAuthService extends AbstractRestService implements IRestService {
 
 	private static final String AUTH_TOKEN_FUNCTION_NAME = "authTokenFunctionName";
 	private static final String AUTH_TOKEN_FUNCTION_NAMESPACE = "authTokenFunctionNamespace";
+
+	private static final String ERROR_EVENT = "errorOutput";
 	private static final String AUTH_TOKEN = "accessToken";
 	private static final String EXPIRES_IN = "expiresIn";
 
@@ -50,49 +55,53 @@ public class RestAuthService extends AbstractRestService implements IRestService
 
 		return FlatMapUtil.flatMapMono(
 
-				() -> {
-					Map<String, Object> connectionDetails = connection.getConnectionDetails();
+						() -> {
+							Map<String, Object> connectionDetails = connection.getConnectionDetails();
 
-					String authTokenFunctionName = (String) connectionDetails.get(AUTH_TOKEN_FUNCTION_NAME);
-					String authTokenFunctionNameSpace = (String) connectionDetails.get(AUTH_TOKEN_FUNCTION_NAMESPACE);
+							String authTokenFunctionName = (String) connectionDetails.get(AUTH_TOKEN_FUNCTION_NAME);
+							String authTokenFunctionNameSpace = (String) connectionDetails.get(AUTH_TOKEN_FUNCTION_NAMESPACE);
 
-					return coreFunctionService.execute(authTokenFunctionNameSpace, authTokenFunctionName,
-							connection.getAppCode(), connection.getClientCode(), null, null);
-				},
+							return coreFunctionService.execute(authTokenFunctionNameSpace, authTokenFunctionName,
+									connection.getAppCode(), connection.getClientCode(), null, null);
+						},
 
-				authTokenOutput -> Mono.just(authTokenOutput.allResults().get(0))
-						.switchIfEmpty(msgService.throwMessage(msg -> new GenericException(HttpStatus.BAD_REQUEST, msg),
-								AbstractMongoMessageResourceService.OBJECT_NOT_FOUND, connection.getName())),
+						authTokenOutput -> Mono.just(authTokenOutput.allResults().get(0)),
 
-				(authTokenOutput, outputResult) -> {
-					Map<String, JsonElement> eventMap = outputResult.getResult();
-					String authToken = eventMap.get(AUTH_TOKEN).getAsString();
-					long expiresIn = eventMap.get(EXPIRES_IN).getAsLong();
+						(authTokenOutput, outputResult) -> {
 
-					return coreTokenDAO.create(new CoreToken()
-							.setClientCode(connection.getClientCode())
-							.setAppCode(connection.getAppCode())
-							.setConnectionName(connection.getName())
-							.setTokenType(CoreTokensTokenType.ACCESS)
-							.setToken(authToken)
-							.setIsRevoked(Boolean.FALSE)
-							.setExpiresAt(LocalDateTime.now().plusSeconds(expiresIn)));
-				},
+							if (outputResult.getName().equals(ERROR_EVENT)) {
+								return msgService.throwMessage(msg -> new GenericException(HttpStatus.BAD_REQUEST, msg),
+										CoreMessageResourceService.NOT_ABLE_TO_CREATE_TOKEN, connection.getName());
+							}
 
-				(authTokenOutput, outputResult, coreToken) ->
-						makeRestCall(connection, request, coreToken.getToken()));
+							Map<String, JsonElement> eventMap = outputResult.getResult();
+							String authToken = eventMap.get(AUTH_TOKEN).getAsString();
+							long expiresIn = eventMap.get(EXPIRES_IN).getAsLong();
+
+							return coreTokenDAO.create(new CoreToken()
+									.setClientCode(connection.getClientCode())
+									.setAppCode(connection.getAppCode())
+									.setConnectionName(connection.getName())
+									.setTokenType(CoreTokensTokenType.ACCESS)
+									.setToken(authToken)
+									.setIsRevoked(Boolean.FALSE)
+									.setExpiresAt(LocalDateTime.now().plusSeconds(expiresIn)));
+						},
+						(authTokenOutput, outputResult, coreToken) ->
+								makeRestCall(connection, request, coreToken.getToken()))
+				.contextWrite(Context.of(LogUtil.METHOD_NAME, "RestAuthService.call"));
 	}
 
 	private Mono<RestResponse> makeRestCall(Connection connection, RestRequest request, String accessToken) {
-
-		HttpHeaders headers = new HttpHeaders();
 
 		Object tokenPrefix = connection.getConnectionDetails().get("headerPrefix");
 
 		String authorizationHeader = (tokenPrefix != null) ? tokenPrefix + " " + accessToken : "Bearer " + accessToken;
 
-		headers.set("Authorization", authorizationHeader);
+		MultiValueMap<String, String> headers = Optional.ofNullable(request.getHeaders())
+				.orElseGet(HttpHeaders::new);
 
+		headers.add("Authorization", authorizationHeader);
 		request.setHeaders(headers);
 
 		return basicRestService.call(connection, request);
