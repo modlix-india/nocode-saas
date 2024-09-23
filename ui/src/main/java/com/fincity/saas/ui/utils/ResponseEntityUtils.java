772ec24867cca4bdc4c26c688221ea300678a61f
Vishwas Saini <vishwas.saini@fincity.com>
2024-09-23 12:43:54 +0530

    Added Path Definitions to apply URIPath based on HttpMethod of the incoming request.

--- ui/src/main/java/com/fincity/saas/ui/utils/ResponseEntityUtils.java
+++ ui/src/main/java/com/fincity/saas/ui/utils/ResponseEntityUtils.java
@@ -15,26 +15,6 @@ public class ResponseEntityUtils {
 
 	public static <T> Mono<ResponseEntity<T>> makeResponseEntity(ObjectWithUniqueID<T> obj, String eTag, int cacheAge) {
 
-		if (eTag != null && (eTag.contains(obj.getUniqueId()) || obj.getUniqueId()
-				.contains(eTag)))
-			return Mono.just(ResponseEntity.status(HttpStatus.NOT_MODIFIED)
-					.build());
-
-		var rp = ResponseEntity.ok()
-				.header("ETag", "W/" + obj.getUniqueId())
-				.header("Cache-Control", "max-age: " + cacheAge + ", must-revalidate")
-				.header("x-frame-options", "SAMEORIGIN")
-				.header("X-Frame-Options", "SAMEORIGIN");
-
-		if (obj.getHeaders() != null) {
-			obj.getHeaders().forEach(rp::header);
-		}
-
-		return Mono.just(rp.body(obj.getObject()));
-	}
-
-	public static <T> Mono<ResponseEntity<T>> makeResponseEntity(ObjectWithUniqueID<T> obj, HttpStatus httpStatus, String eTag, int cacheAge) {
-
 		return makeResponseEntity(obj, eTag, cacheAge, null);
 	}
 
