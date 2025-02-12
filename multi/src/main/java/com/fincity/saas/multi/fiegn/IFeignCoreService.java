928d585587ed11e4cdb6d75cdaff6368c16acb43
Kailash Kumar <kailash.kumar@modlix.com>
2025-02-12 16:23:18 +0530

    pull merge conflicts resolved

--- multi/src/main/java/com/fincity/saas/multi/fiegn/IFeignCoreService.java
+++ multi/src/main/java/com/fincity/saas/multi/fiegn/IFeignCoreService.java
@@ -39,7 +39,6 @@ public interface IFeignCoreService {
                             @RequestHeader("clientCode") String clientCode,
                             @RequestHeader("appCode") String headerAppCode,
                             @RequestParam("deleteAppCode") String deleteAppCode);
-}
 
     @GetMapping("${core.connection.getOAuth2Token:/api/core/connections/oauth2/token/{connectionName}}")
     Mono<String> getConnectionOAuth2Token(
