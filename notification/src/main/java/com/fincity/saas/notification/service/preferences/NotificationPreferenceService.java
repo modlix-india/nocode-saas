2f2d71396e26151798fad26c280d45d0650d8c3a
Vishwas Saini <vishwas.saini@fincity.com>
2025-02-13 13:21:10 +0530

    Completed Template Processor. Added Different Dao and services for notification classes

--- notification/src/main/java/com/fincity/saas/notification/service/preferences/NotificationPreferenceService.java
+++ notification/src/main/java/com/fincity/saas/notification/service/preferences/NotificationPreferenceService.java
@@ -5,18 +5,14 @@ import org.jooq.types.ULong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fincity.nocode.reactor.util.FlatMapUtil;
 import com.fincity.saas.commons.jooq.service.AbstractJOOQUpdatableDataService;
 import com.fincity.saas.commons.security.feign.IFeignSecurityService;
-import com.fincity.saas.commons.security.jwt.ContextAuthentication;
-import com.fincity.saas.commons.security.util.SecurityContextUtil;
 import com.fincity.saas.commons.service.CacheService;
 import com.fincity.saas.notification.dao.preference.NotificationPreferenceDao;
 import com.fincity.saas.notification.dto.prefrence.NotificationPreference;
 import com.fincity.saas.notification.service.NotificationMessageResourceService;
 
 import lombok.Getter;
-import reactor.core.publisher.Mono;
 
 @Service
 public abstract class NotificationPreferenceService<R extends UpdatableRecord<R>, D extends NotificationPreference<D>,
@@ -47,33 +43,6 @@ public abstract class NotificationPreferenceService<R extends UpdatableRecord<R>
 
 	private String getCacheKeys(ULong clientId, ULong identifierId) {
 		return clientId + ":" + identifierId;
-	}
-
-	public Mono<D> create(D entity) {
-
-		return FlatMapUtil.flatMapMono(
-
-				SecurityContextUtil::getUsersContextAuthentication,
-
-		)
-	}
-
-	private Mono<D> updateIdentifiers(ContextAuthentication ca, D entity) {
-
-		if (isAppLevel()) {
-			return FlatMapUtil.flatMapMono(
-
-
-
-			)
-		}
-
-		return FlatMapUtil.flatMapMono(
-				() -> this.securityService.getAppByCode(ca.getUrlAppCode()),
-
-				app ->
-		)
-
 	}
 
 
