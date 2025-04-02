6e85e11700c12e8e5de50b7a5702944522fb3dc3
Vishwas Saini <vishwas.saini@fincity.com>
2025-04-02 16:58:49 +0530

    Initial Commit Entity servers

---
+++ entity-processor/src/main/java/com/fincity/saas/entity/processor/EntityProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.fincity.saas.entity.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityProcessorApplication.class, args);
+	}
+
+}
---
+++ entity-processor/src/test/java/com/fincity/saas/entity/processor/EntityProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincity.saas.entity.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
