038be66ece70bc7c181504e588b739118c482f35
Vishwas Saini <vishwas.saini@fincity.com>
2025-04-03 15:32:24 +0530

    Added basic class structure for JOOQ flow

---
+++ commons-jooq/src/main/java/com/fincity/saas/commons/jooq/flow/TableSchema.java
@@ -0,0 +1,7 @@
+package com.fincity.saas.commons.jooq.flow;
+
+import com.fincity.nocode.kirun.engine.function.system.date.Difference;
+import com.fincity.nocode.kirun.engine.json.schema.Schema;
+
+public class TableSchema extends Schema {
+}
---
+++ commons-jooq/src/main/java/com/fincity/saas/commons/jooq/flow/dao/AbstractFlowUpdatableDAO.java
@@ -0,0 +1,4 @@
+package com.fincity.saas.commons.jooq.flow.dao;
+
+public class AbstractFlowUpdatableDAO {
+}
