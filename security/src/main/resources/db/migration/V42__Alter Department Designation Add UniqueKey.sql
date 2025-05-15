53c1c247185165092c0ecf475b64f3f3defc1e7b
serialcoder13 <kiran@modlix.com>
2025-05-15 11:11:16 +0530

    There are existing duplicates so truncating the data to add the constraint.

--- security/src/main/resources/db/migration/V42__Alter Department Designation Add UniqueKey.sql
+++ security/src/main/resources/db/migration/V42__Alter Department Designation Add UniqueKey.sql
@@ -1,5 +1,9 @@
 USE security;
 
+truncate `security`.`security_department`;
+
+truncate `security`.`security_designation`;
+
 ALTER TABLE `security`.`security_department`
     ADD CONSTRAINT `UK1_SECURITY_DEPARTMENT_CLIENT_ID_NAME` UNIQUE (`CLIENT_ID`, `NAME`);
 
