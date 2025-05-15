5abf51f67ce9ab04c2dc22de7934a1e2ce640a95
Kailash Kumar <kailash.kumar@modlix.com>
2025-05-15 10:29:54 +0530

    added unique key for department and designation for combination of client_id and name

--- seed data/client_setup.sql
+++ seed data/client_setup.sql
@@ -4389,6 +4389,39 @@ values (@v_profile_appbuilder_owner, @v_r_1),
        (@v_profile_appbuilder_owner, @v_r_163),
        (@v_profile_appbuilder_owner, @v_r_164);
 
+-- V41__Security OneTimeToken script.sql
+use security;
+
+DROP TABLE IF EXISTS `security_one_time_token`;
+
+CREATE TABLE `security_one_time_token`
+(
+    `ID`         bigint unsigned                                              NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
+    `USER_ID`    bigint unsigned                                              NOT NULL COMMENT 'User id',
+    `TOKEN`      char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci    NOT NULL COMMENT 'One Time Token',
+    `IP_ADDRESS` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User IP from where he logged in',
+    `CREATED_AT` timestamp                                                    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time when this row is created',
+
+    PRIMARY KEY (`ID`),
+    KEY `FK1_ONE_TIME_TOKEN_USER_ID` (`USER_ID`),
+    UNIQUE KEY `FK3_ONE_TIME_TOKEN` (`TOKEN`),
+    CONSTRAINT `FK2_ONE_TIME_TOKEN_USER_ID` FOREIGN KEY (`USER_ID`) REFERENCES `security_user` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
+
+-- V42__Security Alter Department Designation Add UniqueKey.sql
+
+USE `security`;
+
+ALTER TABLE `security`.`security_department`
+    ADD CONSTRAINT `UK1_SECURITY_DEPARTMENT_CLIENT_ID_NAME` UNIQUE (`CLIENT_ID`, `NAME`);
+
+ALTER TABLE `security`.`security_designation`
+    ADD CONSTRAINT `UK1_SECURITY_DESIGNATION_CLIENT_ID_NAME` UNIQUE (`CLIENT_ID`, `NAME`);
+
 
 -- Add scripts from the project above this line and seed data below this line.
 
