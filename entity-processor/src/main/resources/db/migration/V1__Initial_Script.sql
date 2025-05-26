56674ee2d761a8d9c15bb6c912f03d92d4497902
serialcoder13 <kiran@modlix.com>
2025-05-26 14:06:21 +0530

    Add mysql-connector dependency and disable DB drop in migration
    Added the `mysql-connector-j` dependency in the pom.xml to enable MySQL interactions. Commented out the `DROP DATABASE` statement in the migration script to prevent accidental database deletion.

--- entity-processor/src/main/resources/db/migration/V1__Initial_Script.sql
+++ entity-processor/src/main/resources/db/migration/V1__Initial_Script.sql
@@ -1,6 +1,6 @@
 /* Creating Database. */
 
-DROP DATABASE IF EXISTS `entity_processor`;
+-- DROP DATABASE IF EXISTS `entity_processor`;
 
 CREATE DATABASE IF NOT EXISTS `entity_processor` DEFAULT CHARACTER SET `UTF8MB4` COLLATE `UTF8MB4_UNICODE_CI`;
 
