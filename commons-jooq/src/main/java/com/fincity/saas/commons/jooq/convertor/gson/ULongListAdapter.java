936c783243500f708799b43d39000384b9523f40
lawbringr1 <vishwas.saini@modlix.com>
2025-07-07 21:16:37 +0530

    Fixed List reading fo ULong json. Added markdown features format in Activity

--- commons-jooq/src/main/java/com/fincity/saas/commons/jooq/convertor/gson/ULongListAdapter.java
+++ commons-jooq/src/main/java/com/fincity/saas/commons/jooq/convertor/gson/ULongListAdapter.java
@@ -6,12 +6,12 @@ import com.fincity.saas.commons.jooq.util.ULongUtil;
 
 public class ULongListAdapter extends AbstractListAdapter<ULong> {
 
-	public ULongListAdapter() {
-		super(ULongUtil::valueOf);
-	}
+    public ULongListAdapter() {
+        super(ULongUtil::valueOf);
+    }
 
-	@Override
-	protected String serializeItem(ULong item) {
-		return item.toBigInteger().toString();
-	}
+    @Override
+    protected String serializeItem(ULong item) {
+        return item.toBigInteger().toString();
+    }
 }
