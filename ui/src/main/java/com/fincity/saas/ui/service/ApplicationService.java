fa6db17438dcda2a7f2583771c02e1e646fc071e
serialcoder13 <kiran@modlix.com>
2025-08-11 09:52:36 +0530

    Refactor `ApplicationService` to improve code formatting, readability, and introduce the helper method `processForVariables` for URL processing logic.

--- ui/src/main/java/com/fincity/saas/ui/service/ApplicationService.java
+++ ui/src/main/java/com/fincity/saas/ui/service/ApplicationService.java
@@ -4,7 +4,9 @@ import static com.fincity.nocode.reactor.util.FlatMapUtil.*;
 
 import java.util.Map;
 
+import com.fincity.saas.commons.util.BooleanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +33,9 @@ public class ApplicationService extends AbstractUIOverridableDataService<Applica
 
     private UIFillerService fillerService;
 
+    @Value("${security.appCodeSuffix:}")
+    private String appCodeSuffix;
+
     @Autowired
     public ApplicationService(PageService pageService, UIFillerService fillerService) {
         super(Application.class);
@@ -53,11 +58,11 @@ public class ApplicationService extends AbstractUIOverridableDataService<Applica
     public Mono<Application> create(Application entity) {
 
         if (StringUtil.safeIsBlank(entity.getName()) || StringUtil.safeIsBlank(entity.getAppCode())
-            || !StringUtil.safeEquals(entity.getName(), entity.getAppCode())
-            || !StringUtil.onlyAlphabetAllowed(entity.getAppCode()))
+                || !StringUtil.safeEquals(entity.getName(), entity.getAppCode())
+                || !StringUtil.onlyAlphabetAllowed(entity.getAppCode()))
 
             return this.messageResourceService.throwMessage(msg -> new GenericException(HttpStatus.BAD_REQUEST, msg),
-                UIMessageResourceService.APP_NAME_MISMATCH);
+                    UIMessageResourceService.APP_NAME_MISMATCH);
 
         return super.create(entity);
     }
@@ -66,38 +71,38 @@ public class ApplicationService extends AbstractUIOverridableDataService<Applica
     public Mono<Application> update(Application entity) {
 
         return FlatMapUtil.flatMapMono(
-            () -> super.update(entity),
-            this::evictAll);
+                () -> super.update(entity),
+                this::evictAll);
     }
 
     private Mono<Application> evictAll(Application e) {
 
         return FlatMapUtil.flatMapMono(
-            () -> cacheService.evictAll(
-                this.getCacheName(e.getAppCode() + "_" + IndexHTMLService.CACHE_NAME_INDEX, e.getAppCode())),
-            x -> cacheService.evictAll(
-                this.getCacheName(e.getAppCode() + "_" + ManifestService.CACHE_NAME_MANIFEST, e.getAppCode())),
-            (x, y) -> cacheService
-                .evictAll(this.getCacheName(e.getAppCode() + "_" + CACHE_NAME_PROPERTIES, e.getAppCode())),
-            (x, y, z) -> Mono.just(e));
+                () -> cacheService.evictAll(
+                        this.getCacheName(e.getAppCode() + "_" + IndexHTMLService.CACHE_NAME_INDEX, e.getAppCode())),
+                x -> cacheService.evictAll(
+                        this.getCacheName(e.getAppCode() + "_" + ManifestService.CACHE_NAME_MANIFEST, e.getAppCode())),
+                (x, y) -> cacheService
+                        .evictAll(this.getCacheName(e.getAppCode() + "_" + CACHE_NAME_PROPERTIES, e.getAppCode())),
+                (x, y, z) -> Mono.just(e));
     }
 
     private Mono<Boolean> evictAll(String appCode) {
 
         return FlatMapUtil.flatMapMono(
-            () -> cacheService.evictAll(
-                this.getCacheName(appCode + "_" + IndexHTMLService.CACHE_NAME_INDEX, appCode)),
-            x -> cacheService.evictAll(
-                this.getCacheName(appCode + "_" + ManifestService.CACHE_NAME_MANIFEST, appCode)),
-            (x, y) -> cacheService
-                .evictAll(this.getCacheName(appCode + "_" + CACHE_NAME_PROPERTIES, appCode)));
+                () -> cacheService.evictAll(
+                        this.getCacheName(appCode + "_" + IndexHTMLService.CACHE_NAME_INDEX, appCode)),
+                x -> cacheService.evictAll(
+                        this.getCacheName(appCode + "_" + ManifestService.CACHE_NAME_MANIFEST, appCode)),
+                (x, y) -> cacheService
+                        .evictAll(this.getCacheName(appCode + "_" + CACHE_NAME_PROPERTIES, appCode)));
     }
 
     @Override
     public Mono<Boolean> delete(String id) {
 
         return this.read(id)
-            .flatMap(e -> super.delete(id).flatMap(x -> this.evictAll(e).thenReturn(x)));
+                .flatMap(e -> super.delete(id).flatMap(x -> this.evictAll(e).thenReturn(x)));
     }
 
     @Override
@@ -105,24 +110,24 @@ public class ApplicationService extends AbstractUIOverridableDataService<Applica
 
         return flatMapMono(
 
-            () -> this.read(entity.getId()),
+                () -> this.read(entity.getId()),
 
-            existing -> {
-                if (existing.getVersion() != entity.getVersion())
-                    return this.messageResourceService.throwMessage(
-                        msg -> new GenericException(HttpStatus.PRECONDITION_FAILED, msg),
-                        AbstractMongoMessageResourceService.VERSION_MISMATCH);
+                existing -> {
+                    if (existing.getVersion() != entity.getVersion())
+                        return this.messageResourceService.throwMessage(
+                                msg -> new GenericException(HttpStatus.PRECONDITION_FAILED, msg),
+                                AbstractMongoMessageResourceService.VERSION_MISMATCH);
 
-                existing.setProperties(entity.getProperties())
-                    .setTranslations(entity.getTranslations())
-                    .setLanguages(entity.getLanguages());
+                    existing.setProperties(entity.getProperties())
+                            .setTranslations(entity.getTranslations())
+                            .setLanguages(entity.getLanguages());
 
-                existing.setMessage(entity.getMessage());
-                existing.setDefaultLanguage(entity.getDefaultLanguage());
-                existing.setVersion(existing.getVersion() + 1);
+                    existing.setMessage(entity.getMessage());
+                    existing.setDefaultLanguage(entity.getDefaultLanguage());
+                    existing.setVersion(existing.getVersion() + 1);
 
-                return Mono.just(existing);
-            }).contextWrite(Context.of(LogUtil.METHOD_NAME, "ApplicationService.updatableEntity"));
+                    return Mono.just(existing);
+                }).contextWrite(Context.of(LogUtil.METHOD_NAME, "ApplicationService.updatableEntity"));
     }
 
     public Mono<Map<String, Object>> readProperties(String name, String appCode, String clientCode) { // NOSONAR
@@ -130,60 +135,61 @@ public class ApplicationService extends AbstractUIOverridableDataService<Applica
 
         return FlatMapUtil.flatMapMonoWithNull(
 
-                () -> Mono.just(clientCode),
+                        () -> Mono.just(clientCode),
 
-                key -> cacheService.get(this.getCacheName(appCode + "_" + CACHE_NAME_PROPERTIES, appCode), key)
-                    .map(this.pojoClass::cast),
+                        key -> cacheService.get(this.getCacheName(appCode + "_" + CACHE_NAME_PROPERTIES, appCode), key)
+                                .map(this.pojoClass::cast),
 
-                (key, cApp) -> {
-                    if (cApp != null)
-                        return Mono.just(cApp);
+                        (key, cApp) -> {
+                            if (cApp != null)
+                                return Mono.just(cApp);
 
-                    return SecurityContextUtil.getUsersContextAuthentication()
-                        .flatMap(ca -> this.readIfExistsInBase(name, appCode, ca.getUrlClientCode(),
-                            clientCode));
-                },
+                            return SecurityContextUtil.getUsersContextAuthentication()
+                                    .flatMap(ca -> this.readIfExistsInBase(name, appCode, ca.getUrlClientCode(),
+                                            clientCode));
+                        },
 
-                (key, cApp, dbApp) -> dbApp == null ? Mono.empty() : this.readInternal(dbApp.getId()),
+                        (key, cApp, dbApp) -> dbApp == null ? Mono.empty() : this.readInternal(dbApp.getId()),
 
-                (key, cApp, dbApp, mergedApp) -> {
+                        (key, cApp, dbApp, mergedApp) -> {
 
-                    if (cApp == null && mergedApp == null)
-                        return Mono.empty();
+                            if (cApp == null && mergedApp == null)
+                                return Mono.empty();
 
-                    try {
-                        return Mono.just(this.pojoClass.getConstructor(this.pojoClass)
-                            .newInstance(cApp != null ? cApp : mergedApp));
-                    } catch (Exception e) {
+                            try {
+                                return Mono.just(this.pojoClass.getConstructor(this.pojoClass)
+                                        .newInstance(cApp != null ? cApp : mergedApp));
+                            } catch (Exception e) {
 
-                        return this.messageResourceService.throwMessage(
-                            msg -> new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, msg, e),
-                            AbstractMongoMessageResourceService.UNABLE_TO_CREATE_OBJECT, this.getObjectName());
-                    }
-                },
+                                return this.messageResourceService.throwMessage(
+                                        msg -> new GenericException(HttpStatus.INTERNAL_SERVER_ERROR, msg, e),
+                                        AbstractMongoMessageResourceService.UNABLE_TO_CREATE_OBJECT, this.getObjectName());
+                            }
+                        },
 
-                (key, cApp, dbApp, mergedApp, clonedApp) -> {
+                        (key, cApp, dbApp, mergedApp, clonedApp) -> {
 
-                    if (clonedApp == null)
-                        return Mono.empty();
+                            if (clonedApp == null)
+                                return Mono.empty();
 
-                    if (cApp == null && mergedApp != null) {
-                        cacheService.put(this.getCacheName(appCode + "_" + CACHE_NAME_PROPERTIES, appCode), mergedApp,
-                            key);
-                    }
+                            if (cApp == null && mergedApp != null) {
+                                cacheService.put(this.getCacheName(appCode + "_" + CACHE_NAME_PROPERTIES, appCode), mergedApp,
+                                        key);
+                            }
 
-                    return Mono.justOrEmpty(clonedApp.getProperties());
-                })
-            .contextWrite(Context.of(LogUtil.METHOD_NAME, "ApplicationService.readProperties"))
-            .defaultIfEmpty(Map.of());
+                            return Mono.justOrEmpty(clonedApp.getProperties());
+                        })
+                .contextWrite(Context.of(LogUtil.METHOD_NAME, "ApplicationService.readProperties"))
+                .defaultIfEmpty(Map.of());
     }
 
     @Override
     public Mono<Boolean> deleteEverything(String appCode, String clientCode) {
         return super.deleteEverything(appCode, clientCode)
-            .flatMap(x -> this.evictAll(appCode));
+                .flatMap(x -> this.evictAll(appCode));
     }
 
+    @SuppressWarnings({"unchecked", "raw"})
     @Override
     protected Mono<ObjectWithUniqueID<Application>> applyChange(String name, String appCode, String clientCode,
                                                                 Application object, String id) {
@@ -193,56 +199,90 @@ public class ApplicationService extends AbstractUIOverridableDataService<Applica
 
         return FlatMapUtil.flatMapMonoWithNull(
 
-                SecurityContextUtil::getUsersContextAuthentication,
+                        SecurityContextUtil::getUsersContextAuthentication,
 
-                ca -> Mono.just(ca == null || object.getPermission() == null
-                    || SecurityContextUtil.hasAuthority(object.getPermission(), ca.getAuthorities())),
+                        ca -> Mono.just(ca == null || object.getPermission() == null
+                                || SecurityContextUtil.hasAuthority(object.getPermission(), ca.getAuthorities())),
 
-                (ca, showShellPage) -> {
+                        (ca, showShellPage) -> {
 
-                    Map<String, Object> props = object.getProperties();
+                            Map<String, Object> props = object.getProperties();
 
-                    if (props == null || props.get("shellPage") == null)
-                        return Mono.empty();
+                            if (props == null || props.get("shellPage") == null)
+                                return Mono.empty();
 
-                    Object pageName = props.get("shellPage");
+                            Object pageName = props.get("shellPage");
 
-                    if (!showShellPage.booleanValue() && props.get("forbiddenPage") != null)
-                        pageName = props.get("forbiddenPage");
+                            if (!BooleanUtil.safeValueOf(showShellPage) && props.get("forbiddenPage") != null)
+                                pageName = props.get("forbiddenPage");
 
-                    return this.pageService.read(pageName.toString(), object.getAppCode(), clientCode);
-                },
+                            return this.pageService.read(pageName.toString(), object.getAppCode(), clientCode);
+                        },
 
-                (ca, ssp, shellPage) -> this.fillerService.read(object.getAppCode(), object.getAppCode(), clientCode),
+                        (ca, ssp, shellPage) -> this.fillerService.read(object.getAppCode(), object.getAppCode(), clientCode),
 
-                (ca, ssp, shellPage, filler) -> {
+                        (ca, ssp, shellPage, filler) -> {
 
-                    if (shellPage == null) {
+                            if (object.getProperties().get("mobileApps") != null) {
+                                object.getProperties().remove("mobileApps");
+                            }
 
-                        if (filler == null)
-                            return Mono.just(new ObjectWithUniqueID<>(object, id));
+                            if (object.getProperties().get("manifest") != null) {
+                                object.getProperties().remove("manifest");
+                            }
 
-                        object.getProperties()
-                            .put("fillerValues", filler.getObject().getValues());
-                        return Mono.just(
-                            new ObjectWithUniqueID<>(object, filler == null ? id : id + filler.getUniqueId()));
-                    }
+                            if (object.getProperties().get("sso2") instanceof Map sso2) {
 
-                    StringBuilder sb = new StringBuilder(id);
+                                String url = StringUtil.safeValueOf(sso2.get("urlPrefix"));
+                                if (url != null) {
+                                    sso2.put("urlPrefix", processForVariables(url));
+                                }
+                            }
 
-                    if (filler != null) {
-                        sb.append(filler.getUniqueId());
-                        object.getProperties()
-                            .put("fillerValues", filler.getObject().getValues());
-                    }
+                            if (shellPage == null) {
 
-                    sb.append(shellPage.getUniqueId());
-                    object.getProperties()
-                        .put("shellPageDefinition", shellPage.getObject());
+                                if (filler == null)
+                                    return Mono.just(new ObjectWithUniqueID<>(object, id));
 
-                    return Mono.just(
-                        new ObjectWithUniqueID<>(object, sb.toString()));
-                })
-            .contextWrite(Context.of(LogUtil.METHOD_NAME, "ApplicationService.applyChange"));
+                                object.getProperties()
+                                        .put("fillerValues", filler.getObject().getValues());
+                                return Mono.just(
+                                        new ObjectWithUniqueID<>(object, id + filler.getUniqueId()));
+                            }
+
+                            StringBuilder sb = new StringBuilder(id);
+
+                            if (filler != null) {
+                                sb.append(filler.getUniqueId());
+                                object.getProperties()
+                                        .put("fillerValues", filler.getObject().getValues());
+                            }
+
+                            sb.append(shellPage.getUniqueId());
+                            object.getProperties()
+                                    .put("shellPageDefinition", shellPage.getObject());
+
+                            return Mono.just(
+                                    new ObjectWithUniqueID<>(object, sb.toString()));
+                        })
+                .contextWrite(Context.of(LogUtil.METHOD_NAME, "ApplicationService.applyChange"));
+    }
+
+    public String processForVariables(String url) {
+        if (StringUtil.safeIsBlank(url)) {
+            return url;
+        }
+
+        url = url.replace("{envDotSuffix}", appCodeSuffix);
+
+        String env = StringUtil.safeIsBlank(appCodeSuffix) ? "" : appCodeSuffix.substring(appCodeSuffix.indexOf(".") + 1);
+
+        url = url.replace("{env}", env);
+
+        env = StringUtil.safeIsBlank(env) ? "" : env + ".";
+
+        url = url.replace("{envDotPrefix}", env);
+
+        return url;
     }
 }
