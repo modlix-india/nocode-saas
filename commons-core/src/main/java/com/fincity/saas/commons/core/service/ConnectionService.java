ef55f8b53b86bb7592a4ccf9d6066aa757f50464
lawbringr1 <vishwas.saini@modlix.com>
2025-08-13 15:50:00 +0530

    Changed ConnectionType SMS -> ConnectionType TEXT

--- commons-core/src/main/java/com/fincity/saas/commons/core/service/ConnectionService.java
+++ commons-core/src/main/java/com/fincity/saas/commons/core/service/ConnectionService.java
@@ -25,7 +25,8 @@ import reactor.util.context.Context;
 public class ConnectionService extends AbstractOverridableDataService<Connection, ConnectionRepository> {
 
     @Autowired(required = false)
-    @Qualifier("pubRedisAsyncCommand") private RedisPubSubAsyncCommands<String, String> pubAsyncCommand;
+    @Qualifier("pubRedisAsyncCommand")
+    private RedisPubSubAsyncCommands<String, String> pubAsyncCommand;
 
     @Value("${redis.connection.eviction.channel:connectionChannel}")
     private String channel;
@@ -133,15 +134,13 @@ public class ConnectionService extends AbstractOverridableDataService<Connection
         return FlatMapUtil.flatMapMono(
                 () -> super.readInternal(name, appCode, clientCode).map(ObjectWithUniqueID::getObject),
                 conn -> {
-	                if (conn.getConnectionType() == type) return Mono.justOrEmpty(conn);
-	                return Mono.justOrEmpty((Connection) null);
+                    if (conn.getConnectionType() == type) return Mono.justOrEmpty(conn);
+                    return Mono.empty();
                 },
                 (conn, typedConn) -> {
-	                if (typedConn.getClientCode().equals(clientCode)
-			                || BooleanUtil.safeValueOf(typedConn.getIsAppLevel())) return Mono.justOrEmpty(
-			                typedConn);
-	                return Mono.justOrEmpty(
-			                (Connection) null);
+                    if (typedConn.getClientCode().equals(clientCode)
+                            || BooleanUtil.safeValueOf(typedConn.getIsAppLevel())) return Mono.justOrEmpty(typedConn);
+                    return Mono.empty();
                 });
     }
 }
