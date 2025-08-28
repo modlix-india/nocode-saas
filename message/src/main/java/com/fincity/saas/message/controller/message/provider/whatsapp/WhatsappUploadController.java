34e86162f79a03051d3a0e32ab3629ea1dea9f4b
lawbringr1 <vishwas.saini@modlix.com>
2025-08-28 14:01:48 +0530

    Added Json parsing for @RequestPart for upload

--- message/src/main/java/com/fincity/saas/message/controller/message/provider/whatsapp/WhatsappUploadController.java
+++ message/src/main/java/com/fincity/saas/message/controller/message/provider/whatsapp/WhatsappUploadController.java
@@ -1,14 +1,5 @@
 package com.fincity.saas.message.controller.message.provider.whatsapp;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fincity.saas.commons.exeception.GenericException;
 import com.fincity.saas.message.model.message.whatsapp.graph.FileHandle;
@@ -17,7 +8,14 @@ import com.fincity.saas.message.model.message.whatsapp.graph.UploadStatus;
 import com.fincity.saas.message.model.request.message.provider.whatsapp.graph.UploadRequest;
 import com.fincity.saas.message.model.request.message.provider.whatsapp.graph.UploadSessionRequest;
 import com.fincity.saas.message.service.message.provider.whatsapp.WhatsappUploadService;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -41,14 +39,11 @@ public class WhatsappUploadController {
 
     @PostMapping
     public Mono<ResponseEntity<FileHandle>> startOrResumeUpload(
-            @RequestPart(name = "file") Mono<FilePart> filePart, @RequestPart(name = "request") String uploadRequest) {
-        try {
-            UploadRequest requestObject = objectMapper.readValue(uploadRequest, UploadRequest.class);
-            return this.service.startOrResumeUpload(requestObject, filePart).map(ResponseEntity::ok);
-        } catch (Exception e) {
-            throw new GenericException(
-                    HttpStatus.BAD_REQUEST, "Failed to deserialize uploadRequest into UploadRequest object", e);
-        }
+            @RequestPart(name = "file") Mono<FilePart> filePart,
+            @RequestPart(name = "uploadRequestString") String uploadRequestString) {
+        return this.service
+                .startOrResumeUpload(this.toUploadRequest(uploadRequestString), filePart)
+                .map(ResponseEntity::ok);
     }
 
     @PostMapping("/status")
@@ -58,13 +53,19 @@ public class WhatsappUploadController {
 
     @PostMapping("/resume")
     public Mono<ResponseEntity<FileHandle>> resumeUploadFromStatus(
-            @RequestPart(name = "file") Mono<FilePart> filePart, @RequestPart(name = "request") String uploadRequest) {
+            @RequestPart(name = "file") Mono<FilePart> filePart,
+            @RequestPart(name = "uploadRequestString") String uploadRequestString) {
+        return this.service
+                .startOrResumeUpload(this.toUploadRequest(uploadRequestString), filePart)
+                .map(ResponseEntity::ok);
+    }
+
+    private UploadRequest toUploadRequest(String uploadRequest) {
         try {
-            UploadRequest requestObject = objectMapper.readValue(uploadRequest, UploadRequest.class);
-            return this.service.startOrResumeUpload(requestObject, filePart).map(ResponseEntity::ok);
+            return objectMapper.readValue(uploadRequest, UploadRequest.class);
         } catch (Exception e) {
             throw new GenericException(
-                    HttpStatus.BAD_REQUEST, "Failed to deserialize uploadRequest into UploadRequest object", e);
+                    HttpStatus.BAD_REQUEST, "Failed to parse uploadRequest into UploadRequest object", e);
         }
     }
 }
