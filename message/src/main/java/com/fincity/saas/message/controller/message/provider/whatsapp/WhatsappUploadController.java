91544328a38dbfaa5b83d4b7473723f9c234bdf6
lawbringr1 <vishwas.saini@modlix.com>
2025-08-28 13:59:48 +0530

    Added Json parsing for @RequestPart for upload

--- message/src/main/java/com/fincity/saas/message/controller/message/provider/whatsapp/WhatsappUploadController.java
+++ message/src/main/java/com/fincity/saas/message/controller/message/provider/whatsapp/WhatsappUploadController.java
@@ -1,13 +1,6 @@
 package com.fincity.saas.message.controller.message.provider.whatsapp;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fincity.saas.message.model.message.whatsapp.graph.FileHandle;
-import com.fincity.saas.message.model.message.whatsapp.graph.UploadSessionId;
-import com.fincity.saas.message.model.message.whatsapp.graph.UploadStatus;
-import com.fincity.saas.message.model.request.message.provider.whatsapp.graph.UploadRequest;
-import com.fincity.saas.message.model.request.message.provider.whatsapp.graph.UploadSessionRequest;
-import com.fincity.saas.message.service.message.provider.whatsapp.WhatsappUploadService;
-import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fincity.saas.commons.exeception.GenericException;
+import com.fincity.saas.message.model.message.whatsapp.graph.FileHandle;
+import com.fincity.saas.message.model.message.whatsapp.graph.UploadSessionId;
+import com.fincity.saas.message.model.message.whatsapp.graph.UploadStatus;
+import com.fincity.saas.message.model.request.message.provider.whatsapp.graph.UploadRequest;
+import com.fincity.saas.message.model.request.message.provider.whatsapp.graph.UploadSessionRequest;
+import com.fincity.saas.message.service.message.provider.whatsapp.WhatsappUploadService;
+
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -43,7 +46,8 @@ public class WhatsappUploadController {
             UploadRequest requestObject = objectMapper.readValue(uploadRequest, UploadRequest.class);
             return this.service.startOrResumeUpload(requestObject, filePart).map(ResponseEntity::ok);
         } catch (Exception e) {
-            return Mono.just(ResponseEntity.badRequest().build());
+            throw new GenericException(
+                    HttpStatus.BAD_REQUEST, "Failed to deserialize uploadRequest into UploadRequest object", e);
         }
     }
 
@@ -59,7 +63,8 @@ public class WhatsappUploadController {
             UploadRequest requestObject = objectMapper.readValue(uploadRequest, UploadRequest.class);
             return this.service.startOrResumeUpload(requestObject, filePart).map(ResponseEntity::ok);
         } catch (Exception e) {
-            return Mono.just(ResponseEntity.badRequest().build());
+            throw new GenericException(
+                    HttpStatus.BAD_REQUEST, "Failed to deserialize uploadRequest into UploadRequest object", e);
         }
     }
 }
