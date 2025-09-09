d8a1c2ef19aa97a2867bbeb9920c35db7fe44281
serialcoder13 <kiran@modlix.com>
2025-09-09 17:58:04 +0530

    Redoing Notifications

---
+++ notification/src/test/java/com/modlix/saas/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modlix.saas.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
