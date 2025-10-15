ea92aa4ba8ba5265e26e0992e46dc0284a2273f8
serialcoder13 <kiran@modlix.com>
2025-10-15 13:13:03 +0530

    Add billing and payment DTOs for invoice management
    - Introduced new classes: Invoice, InvoiceItem, Payment, PaymentGateway, ClientPlan, Plan, PlanCycle, and PlanLimit to support billing and payment functionalities.
    - Each class extends AbstractUpdatableDTO and includes relevant fields for managing invoices, payments, plans, and their associated items and cycles.
    - Implemented necessary imports and annotations for data handling and serialization.
    - Established a structured approach to manage billing-related data within the security schema.

---
+++ security/src/main/java/com/fincity/security/dto/plansnbilling/PlanCycle.java
@@ -0,0 +1,44 @@
+package com.fincity.security.dto.plansnbilling;
+
+import java.io.Serial;
+import java.math.BigDecimal;
+
+import org.jooq.types.ULong;
+
+import com.fincity.saas.commons.model.dto.AbstractUpdatableDTO;
+import com.fincity.security.jooq.enums.SecurityPlanCycleStatus;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class PlanCycle extends AbstractUpdatableDTO<ULong, ULong> {
+    
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String description;
+    private ULong planId;
+    private BigDecimal cost;
+    private String currency;
+
+    private BigDecimal tax1;
+    private String tax1Name;
+    private BigDecimal tax2;
+    private String tax2Name;
+    private BigDecimal tax3;
+    private String tax3Name;
+    private BigDecimal tax4;
+    private String tax4Name;
+    private BigDecimal tax5;
+    private String tax5Name;
+
+    private Integer interval;
+    private SecurityPlanCycleStatus status;
+}
