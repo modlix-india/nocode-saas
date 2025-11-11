89d2e3dadc1c073513079f212acce327ae58b76c
lawbringr1 <vishwas.saini@modlix.com>
2025-11-11 10:12:11 +0530

    Only reassigning when the stage is changed for a ticket

--- entity-processor/src/main/java/com/fincity/saas/entity/processor/service/TicketService.java
+++ entity-processor/src/main/java/com/fincity/saas/entity/processor/service/TicketService.java
@@ -35,7 +35,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
-import reactor.util.function.Tuple2;
 
 @Service
 public class TicketService extends BaseProcessorService<EntityProcessorTicketsRecord, Ticket, TicketDAO> {
