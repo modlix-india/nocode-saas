dd9200e812b548175fc301810316649150a4cef1
lawbringr1 <vishwas.saini@modlix.com>
2025-11-24 16:40:42 +0530

    Added appCode in user fetching in walkin form

--- entity-processor/src/main/java/com/fincity/saas/entity/processor/service/form/ProductWalkInFormService.java
+++ entity-processor/src/main/java/com/fincity/saas/entity/processor/service/form/ProductWalkInFormService.java
@@ -7,7 +7,6 @@ import com.fincity.saas.commons.util.BooleanUtil;
 import com.fincity.saas.commons.util.LogUtil;
 import com.fincity.saas.entity.processor.dao.form.ProductWalkInFormDAO;
 import com.fincity.saas.entity.processor.dto.Ticket;
-import com.fincity.saas.entity.processor.dto.base.BaseDto;
 import com.fincity.saas.entity.processor.dto.form.ProductWalkInForm;
 import com.fincity.saas.entity.processor.dto.product.Product;
 import com.fincity.saas.entity.processor.enums.AssignmentType;
@@ -140,8 +139,8 @@ public class ProductWalkInFormService
                 (client, hasAccess) -> {
                     MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
                     queryParams.add(User.Fields.statusCode, "ACTIVE");
-					queryParams.add("appCode", appCode);
-					queryParams.add("fetchProfiles", Boolean.TRUE.toString());
+                    queryParams.add("appCode", appCode);
+                    queryParams.add("fetchProfiles", Boolean.TRUE.toString());
                     return super.securityService.getClientUserInternal(List.of(client.getId()), queryParams);
                 });
     }
